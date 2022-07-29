c8c650cc16b163db56cb1aaca684042b53d62487
lilu <admin@lilu.org.cn>
2022-07-29 11:56:35 +0800

    [feat] set zoneId and refactor

--- src/main/java/com/sunchaser/dog/box/DogBox.java
+++ src/main/java/com/sunchaser/dog/box/DogBox.java
@@ -1,11 +1,15 @@
 package com.sunchaser.dog.box;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.text.CharPool;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.github.GHGist;
 import org.kohsuke.github.GitHubBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,9 +18,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.StringJoiner;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.sunchaser.dog.box.DogBox.EnvEnum.*;
 
 /**
  * dog-box
@@ -26,54 +33,127 @@ import java.util.StringJoiner;
  */
 public class DogBox {
 
-    private static final String[] WEEK = {"一", "二", "三", "四", "五", "六", "日"};
+    private static final Logger LOGGER = LoggerFactory.getLogger(DogBox.class);
 
-    public static void main(String[] args) throws Exception {
-        String ghToken = System.getenv("GH_TOKEN");
-        String gistId = System.getenv("DOG_GIST_ID");
-        String dogWidth = Optional.ofNullable(System.getenv("DOG_WIDTH")).orElse("65");
-        String dogFillLen = Optional.ofNullable(System.getenv("DOG_FILL_LEN")).orElse("106");
-        System.out.println("ghToken=" + ghToken);
-        System.out.println("gistId=" + gistId);
-        GHGist gist = getGhGist(ghToken, gistId);
-        String fileName = "🐶 舔狗日记";
-        String data = getDogContent();
-        String format = getFormatContext(data, Integer.parseInt(dogWidth), Integer.parseInt(dogFillLen));
-        writeToGist(gist, fileName, format);
-        writeToMarkdown(gistId, fileName, format);
+    private static final String DOG_BOX = "🐶 舔狗日记";
+
+    enum WeekEnum {
+
+        ONE("一"),
+
+        TWO("二"),
+
+        THREE("三"),
+
+        FORE("四"),
+
+        FIVE("五"),
+
+        SIX("六"),
+
+        SEVEN("日"),
+
+        ;
+
+        private final String week;
+
+        private static final Map<Integer, WeekEnum> ORDINAL_MAP;
+
+        static {
+           ORDINAL_MAP = Arrays.stream(WeekEnum.values())
+                   .collect(Collectors.toMap(WeekEnum::ordinal, Function.identity()));
+        }
+
+        public static String toChinese(Integer dayOfWeek) {
+            return "星期" + ORDINAL_MAP.get(dayOfWeek - 1).getWeek();
+        }
+
+        WeekEnum(String week) {
+            this.week = week;
+        }
+
+        public String getWeek() {
+            return week;
+        }
     }
 
-    private static void writeToMarkdown(String gistId, String title, String format) {
-        String markdownFile = System.getenv("MARKDOWN_FILE");
-        String startTag = Optional.ofNullable(System.getenv("DOG_BOX_START_TAG")).orElse("<!-- dog-box start -->");
-        String endTag = Optional.ofNullable(System.getenv("DOG_BOX_END_TAG")).orElse("<!-- dog-box end -->");
-        System.out.println(markdownFile);
-        System.out.println(startTag);
-        System.out.println(endTag);
+    enum EnvEnum {
+        GH_TOKEN(StringUtils.EMPTY),
+
+        DOG_GIST_ID("e85d2e5765110be1d8cfe57f2557a130"),
+
+        DOG_WIDTH("65"),
+
+        DOG_FILL_LEN("106"),
+
+        MARKDOWN_FILE("README.md"),
+
+        DOG_BOX_START_TAG("<!-- dog-box start -->"),
+
+        DOG_BOX_END_TAG("<!-- dog-box end -->"),
+
+        TIMEZONE("Asia/Shanghai"),
+
+        ;
+        private final String defaultValue;
+
+        EnvEnum(String defaultValue) {
+            this.defaultValue = defaultValue;
+        }
+
+        public String getDefaultValue() {
+            return defaultValue;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String ghToken = getenv(GH_TOKEN);
+        String gistId = getenv(DOG_GIST_ID);
+        String dogWidth = getenv(DOG_WIDTH);
+        String dogFillLen = getenv(DOG_FILL_LEN);
+        LOGGER.info("ghToken: {}\ngistId: {}\ndogWidth: {}\ndogFillLen: {}", ghToken, gistId, dogWidth, dogFillLen);
+        GHGist gist = getGhGist(ghToken, gistId);
+        String data = getDogContent();
+        String format = getFormatContext(data, Integer.parseInt(dogWidth), Integer.parseInt(dogFillLen));
+        writeToGist(gist, format);
+        writeToMarkdown(gistId, format);
+    }
+
+    private static String getenv(EnvEnum envEnum) {
+        return Optional.ofNullable(System.getenv(envEnum.name())).orElse(envEnum.getDefaultValue());
+    }
+
+    private static void writeToMarkdown(String gistId, String format) {
+        String markdownFile = getenv(MARKDOWN_FILE);
+        String startTag = getenv(DOG_BOX_START_TAG);
+        String endTag = getenv(DOG_BOX_END_TAG);
+        LOGGER.info("markdownFile: {}\nstartTag: {}\nendTag: {}", markdownFile, startTag, endTag);
         if (Objects.nonNull(markdownFile)) {
             String old = FileUtil.readUtf8String(markdownFile);
             int startIndex = old.indexOf(startTag) + startTag.length();
             int endIndex = old.indexOf(endTag);
-            String markdownTitle = StrUtil.format("\n#### <a href=\"https://gist.github.com/{}\" target=\"_blank\">{}</a>\n", gistId, title);
+            String markdownTitle = StrUtil.format("\n#### <a href=\"https://gist.github.com/{}\" target=\"_blank\">{}</a>\n", gistId, DOG_BOX);
             String markdown = old.substring(0, startIndex) + markdownTitle + "```text\n" + format + "\n```\n" + old.substring(endIndex);
             FileUtil.writeUtf8String(markdown, markdownFile);
         }
     }
 
-    private static void writeToGist(GHGist gist, String fileName, String format) throws IOException {
+    private static void writeToGist(GHGist gist, String format) throws IOException {
         gist.update()
-                .updateFile(fileName, format)
+                .updateFile(DOG_BOX, format)
                 .update();
     }
 
     private static String getFormatContext(String data, Integer width, Integer fillLen) {
-        LocalDate now = LocalDate.now();
+        String timeZone = getenv(TIMEZONE);
+        LOGGER.info("timeZone: {}", timeZone);
+        LocalDate now = LocalDate.now(ZoneId.of(timeZone));
         String[] chunks = data.split("(?<=\\G.{" + width + "})");
         StringJoiner joiner = new StringJoiner("\n");
         for (String chunk : chunks) {
             joiner.add(chunk);
         }
-        String date = StrUtil.fillBefore(StrUtil.format("{} 星期{}", now, WEEK[now.getDayOfWeek().getValue() - 1]), ' ', fillLen);
+        String date = StrUtil.fillBefore(StrUtil.format("{} {}", now, WeekEnum.toChinese(now.getDayOfWeek().getValue())), CharPool.SPACE, fillLen);
         return StrUtil.format("{}\n\n{}", joiner, date);
     }
 
